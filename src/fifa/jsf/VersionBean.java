1ce6e2d1dc9f2ff44a24a31a9794ca4e9e2ca5ce
Nick <nick_wilson@yahoo.com>
2019-06-27 12:24:11 -0600

    Using better decompiler now!

--- src/fifa/jsf/VersionBean.java
+++ src/fifa/jsf/VersionBean.java
@@ -1,200 +1,225 @@
-// Decompiled by Jad v1.5.8g. Copyright 2001 Pavel Kouznetsov.
-// Jad home page: http://www.kpdus.com/jad.html
-// Decompiler options: packimports(3) 
-// Source File Name:   VersionBean.java
-
 package fifa.jsf;
 
 import com.mysql.jdbc.StringUtils;
+import fifa.facts.FactBean;
+import fifa.facts.dao.FactDao;
 import fifa.facts.utilities.FactUtilities;
-import fifa.utilities.*;
+import fifa.utilities.FIFAConstants;
+import fifa.utilities.NavigationUtilities;
+import fifa.utilities.PropertiesUtilities;
 import java.io.Serializable;
-import java.util.*;
-import javax.faces.context.ExternalContext;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
 import javax.faces.event.AbortProcessingException;
 import javax.faces.event.ValueChangeEvent;
 import javax.faces.model.SelectItem;
 import javax.servlet.http.HttpSession;
 
-import static fifa.utilities.PropertiesUtilities.*;
 
+
+@ManagedBean(name = "versionBean")
+@ApplicationScoped
 public class VersionBean
-    implements Serializable, FIFAConstants
+        implements Serializable, FIFAConstants
 {
+    private static final long serialVersionUID = 1L;
+    private List<SelectItem> versionSelectItems = loadVersionSelectItems();
+    private final String FIFA = "FIFA";
+    private final String ALL = "ALL";
 
-    public VersionBean()
-    {
-        versionSelectItems = loadVersionSelectItems();
+
+    public String getVersionId() {
+        resetSessionAttributes();
+
+        PropertiesUtilities propertiesUtilities = PropertiesUtilities.getInstance();
+
+        return propertiesUtilities.getProperty(propertiesUtilities.getMessageResource(), "defaultVersion");
     }
 
-    public String getVersionId()
-    {
-        resetSessionAttributes();
-        PropertiesUtilities propertiesUtilities = PropertiesUtilities.getInstance();
-        String versionId = propertiesUtilities.getProperty(propertiesUtilities.getMessageResource(), "defaultVersion");
-        return versionId;
-    }
 
-    public String getVersionIdNotAll()
-    {
+
+
+    public String getVersionIdNotAll() {
         resetSessionAttributes();
+
         PropertiesUtilities propertiesUtilities = PropertiesUtilities.getInstance();
+
         String versionId = propertiesUtilities.getProperty(propertiesUtilities.getMessageResource(), "defaultVersion");
-        if(versionId == null || versionId.equalsIgnoreCase("ALL"))
+
+        if (versionId == null || versionId.equalsIgnoreCase("ALL"))
+        {
+
             versionId = propertiesUtilities.getProperty(propertiesUtilities.getMessageResource(), "latestVersion");
+        }
+
         return versionId;
     }
 
-    private void resetSessionAttributes()
-    {
+    private void resetSessionAttributes() {
         FacesContext facesContext = FacesContext.getCurrentInstance();
         HttpSession session = (HttpSession)facesContext.getExternalContext().getSession(false);
+
+
+
         session.setAttribute("playersRestrictRows", Integer.valueOf(200));
         session.setAttribute("statsRestrictRows", Integer.valueOf(10));
         session.setAttribute("teamsRestrictRows", Integer.valueOf(50));
         session.setAttribute("countriesRestrictRows", Integer.valueOf(50));
     }
 
-    public void setVersionId(String s)
-    {
-    }
 
-    public void versionIdChanged(ValueChangeEvent arg0)
-        throws AbortProcessingException
-    {
+
+
+    public void setVersionId(String versionId) {}
+
+
+
+    public void versionIdChanged(ValueChangeEvent arg0) throws AbortProcessingException {
         String versionId = arg0.getNewValue().toString();
+
         PropertiesUtilities propertiesUtilities = PropertiesUtilities.getInstance();
+
         propertiesUtilities.setMessageProperty(propertiesUtilities.getMessageResource(), "defaultVersion", versionId);
+
         NavigationUtilities navigationUtilities = NavigationUtilities.getInstance();
         navigationUtilities.goHome();
     }
 
-    public String getDatabaseEnvironment()
-    {
+
+    public String getDatabaseEnvironment() {
         PropertiesUtilities propertiesUtilities = PropertiesUtilities.getInstance();
         return propertiesUtilities.getDatabaseEnvironment();
     }
 
-    public void buildAllFactsInteractive()
-    {
-        buildAllFacts(true);
-    }
 
-    public void buildAllFacts(boolean interactive)
-    {
+    public void buildAllFactsInteractive() { buildAllFacts(true); }
+
+
+    public void buildAllFacts(boolean interactive) {
         FactUtilities factUtilities = FactUtilities.getInstance();
         factUtilities.buildAllFacts(interactive);
     }
 
-    public String getLastFactBuildDate()
-    {
+    public String getLastFactBuildDate() {
         FactUtilities factUtilities = FactUtilities.getInstance();
         return factUtilities.getLastFactBuildDate();
     }
 
-    public List getFacts()
-    {
+    public List<FactBean> getFacts() {
         FactDao factDao = new FactDao();
-        List facts = factDao.getFacts();
+        List<FactBean> facts = factDao.getFacts();
+
         long seed = System.nanoTime();
         Collections.shuffle(facts, new Random(seed));
+
         return facts;
     }
 
-    public void doVersionToggle()
-    {
+
+    public void doVersionToggle() {
         FacesContext context = FacesContext.getCurrentInstance();
         HttpSession session = (HttpSession)context.getExternalContext().getSession(true);
+
         String factVersionId = getNextVersionId();
+
         session.setAttribute("factVersion", factVersionId);
     }
 
-    public String getNextVersionId()
-    {
+    public String getNextVersionId() {
         FacesContext context = FacesContext.getCurrentInstance();
-        HttpSession session = (HttpSession)context.getExternalContext().getSession(true);
+        HttpSession session = (HttpSession)context.getExternalContext()
+                .getSession(true);
         String factVersionId = (String)session.getAttribute("factVersion");
-        PropertiesUtilities propertiesUtilities = PropertiesUtilities.getInstance();
-        if(factVersionId != null && factVersionId.equals("ALL"))
-        {
+        PropertiesUtilities propertiesUtilities =
+                PropertiesUtilities.getInstance();
+
+        if (factVersionId != null && factVersionId.equals("ALL")) {
             factVersionId = "FIFA12";
-        } else
-        {
-            if(StringUtils.isNullOrEmpty(factVersionId))
-                factVersionId = propertiesUtilities.getProperty(propertiesUtilities.getMessageResource(), "defaultVersion");
-            if(factVersionId != null && factVersionId.equals("ALL"))
-            {
+
+        }
+        else {
+
+            if (StringUtils.isNullOrEmpty(factVersionId)) {
+                factVersionId = propertiesUtilities.getProperty(
+                        propertiesUtilities.getMessageResource(),
+                        "defaultVersion");
+            }
+
+            if (factVersionId != null && factVersionId.equals("ALL")) {
                 factVersionId = "FIFA12";
-            } else
-            {
+            } else {
                 Integer versionNumber = Integer.valueOf(0);
-                if(factVersionId != null)
-                {
-                    String versionNumberString = factVersionId.length() <= 2 ? factVersionId : factVersionId.substring(factVersionId.length() - 2);
+                if (factVersionId != null) {
+                    String versionNumberString = (factVersionId.length() > 2) ? factVersionId
+                            .substring(factVersionId.length() - 2) : factVersionId;
                     versionNumber = Integer.valueOf(Integer.parseInt(versionNumberString));
                 }
+
                 int latestVersionNumber = getVersionNumber("lastest");
-                if(versionNumber.intValue() < latestVersionNumber)
-                {
+
+                if (versionNumber.intValue() < latestVersionNumber) {
                     versionNumber = Integer.valueOf(versionNumber.intValue() + 1);
-                } else
-                {
+                } else {
                     versionNumber = Integer.valueOf(0);
                     factVersionId = "ALL";
                 }
-                if(versionNumber.intValue() > 0)
-                    factVersionId = (new StringBuilder("FIFA")).append(versionNumber.toString()).toString();
+                if (versionNumber.intValue() > 0) {
+                    factVersionId = "FIFA" + versionNumber.toString();
+                }
             }
         }
         return factVersionId;
     }
 
-    public List getVersionSelectItems()
-    {
-        return versionSelectItems;
-    }
 
-    public void setVersionSelectItems(List versionSelectItems)
-    {
-        this.versionSelectItems = versionSelectItems;
-    }
+    public List<SelectItem> getVersionSelectItems() { return this.versionSelectItems; }
 
-    private List loadVersionSelectItems()
-    {
+
+
+    public void setVersionSelectItems(List<SelectItem> versionSelectItems) { this.versionSelectItems = versionSelectItems; }
+
+
+    private List<SelectItem> loadVersionSelectItems() {
         int earliestVersionNumber = getVersionNumber("earliest");
         int latestVersionNumber = getVersionNumber("latest");
-        versionSelectItems = new ArrayList();
+
+        this.versionSelectItems = new ArrayList();
+
         SelectItem item = new SelectItem("ALL", "All versions");
-        versionSelectItems.add(item);
-        for(Integer x = Integer.valueOf(earliestVersionNumber); x.intValue() <= latestVersionNumber; x = Integer.valueOf(x.intValue() + 1))
-        {
-            item = new SelectItem((new StringBuilder("FIFA")).append(x.toString()).toString(), (new StringBuilder("FIFA ")).append(x.toString()).toString());
-            versionSelectItems.add(item);
+        this.versionSelectItems.add(item);
+
+
+        for (Integer x = Integer.valueOf(earliestVersionNumber); x.intValue() <= latestVersionNumber; x = Integer.valueOf(x.intValue() + 1)) {
+            item = new SelectItem("FIFA" + x.toString(), "FIFA " + x.toString());
+            this.versionSelectItems.add(item);
         }
 
-        return versionSelectItems;
+        return this.versionSelectItems;
     }
 
-    private int getVersionNumber(String versionType)
-    {
+
+    private int getVersionNumber(String versionType) {
         PropertiesUtilities propertiesUtilities = PropertiesUtilities.getInstance();
         String versionId = null;
-        if(versionType.equalsIgnoreCase("latest"))
+        if (versionType.equalsIgnoreCase("latest")) {
             versionId = propertiesUtilities.getProperty(propertiesUtilities.getMessageResource(), "latestVersion");
-        else
-            versionId = "FIFA12";
-        String versionNumberString = "";
-        if(StringUtils.isEmptyOrWhitespaceOnly(versionId))
-            versionNumberString = "0";
-        else
-            versionNumberString = versionId.length() <= 2 ? versionId : versionId.substring(versionId.length() - 2);
-        int versionNumber = Integer.parseInt(versionNumberString);
-        return versionNumber;
-    }
+        } else {
 
-    private static final long serialVersionUID = 1L;
-    private List versionSelectItems;
-    private final String FIFA = "FIFA";
-    private final String ALL = "ALL";
+            versionId = "FIFA12";
+        }
+
+        String versionNumberString = "";
+        if (StringUtils.isEmptyOrWhitespaceOnly(versionId)) {
+            versionNumberString = "0";
+        } else {
+            versionNumberString = (versionId.length() > 2) ? versionId.substring(versionId.length() - 2) :
+                    versionId;
+        }
+        return Integer.parseInt(versionNumberString);
+    }
 }
--- src/fifa/utilities/PropertiesUtilities.java
+++ src/fifa/utilities/PropertiesUtilities.java
@@ -1,204 +1,205 @@
-// Decompiled by Jad v1.5.8g. Copyright 2001 Pavel Kouznetsov.
-// Jad home page: http://www.kpdus.com/jad.html
-// Decompiler options: packimports(3) 
-// Source File Name:   PropertiesUtilities.java
-
 package fifa.utilities;
 
 import com.mysql.jdbc.StringUtils;
 import fifa.jsf.VersionBean;
-import java.io.PrintStream;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.*;
-import javax.faces.application.Application;
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 import javax.faces.context.FacesContext;
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
 
-// Referenced classes of package fifa.utilities:
-//            FIFAConstants
+
+
+
+
 
 public class PropertiesUtilities
-    implements FIFAConstants
+        implements FIFAConstants
 {
+    private static PropertiesUtilities instance = null;
 
-    protected PropertiesUtilities()
-    {
-    }
 
-    public static PropertiesUtilities getInstance()
-    {
-        if(instance == null)
-        {
+
+
+
+    public static PropertiesUtilities getInstance() {
+        if (instance == null) {
             instance = new PropertiesUtilities();
             autoBuildFacts();
         }
         return instance;
     }
 
-    public String getProperty(String propsFile, String propertyKey)
-    {
+
+
+
+
+
+
+    public String getProperty(String propsFile, String propertyKey) {
         PropertiesConfiguration config = null;
         String propertyValue = null;
         Object propertyValueObject = null;
-        ArrayList propertyValueList = null;
-        try
-        {
+        ArrayList<String> propertyValueList = null;
+        try {
             config = new PropertiesConfiguration(propsFile);
             propertyValueObject = config.getProperty(propertyKey);
-            if(propertyValueObject != null)
-                if(propertyValueObject instanceof ArrayList)
-                {
-                    propertyValueList = (ArrayList)config.getProperty(propertyKey);
-                    for(int x = 0; x <= propertyValueList.size() - 1; x++)
-                        if(x == 0)
-                        {
-                            propertyValue = (String)propertyValueList.get(x);
-                            propertyValue = (new StringBuilder(String.valueOf(propertyValue))).append(", ").toString();
-                        } else
-                        {
-                            propertyValue = (new StringBuilder(String.valueOf(propertyValue))).append((String)propertyValueList.get(x)).toString();
-                            if(x < propertyValueList.size() - 1)
-                                propertyValue = (new StringBuilder(String.valueOf(propertyValue))).append(", ").toString();
-                        }
 
-                } else
-                {
+            if (propertyValueObject != null)
+            {
+                if (propertyValueObject instanceof ArrayList) {
+                    propertyValueList = (ArrayList)config.getProperty(propertyKey);
+                    for (int x = 0; x <= propertyValueList.size() - 1; x++) {
+                        if (x == 0) {
+                            propertyValue = (String)propertyValueList.get(x);
+                            propertyValue = String.valueOf(propertyValue) + ", ";
+                        } else {
+                            propertyValue = String.valueOf(propertyValue) + (String)propertyValueList.get(x);
+                            if (x < propertyValueList.size() - 1) {
+                                propertyValue = String.valueOf(propertyValue) + ", ";
+                            }
+                        }
+                    }
+                } else {
+
                     propertyValue = (String)propertyValueObject;
                 }
-        }
-        catch(ConfigurationException e)
-        {
-            System.err.println((new StringBuilder("Error occured getting property ")).append(propertyKey).append(" from file ").append(propsFile).append(". ").append(e.getLocalizedMessage()).toString());
+
+            }
+        } catch (ConfigurationException e) {
+            System.err.println("Error occured getting property " + propertyKey + " from file " + propsFile + ". " +
+                    e.getLocalizedMessage());
             System.err.println(e.getLocalizedMessage());
         }
+
         return propertyValue;
     }
 
-    public void setMessageProperty(String propsFile, String propertyKey, String propertyValue)
-    {
+
+
+
+
+
+
+
+
+
+    public void setMessageProperty(String propsFile, String propertyKey, String propertyValue) {
         PropertiesConfiguration config = null;
-        try
-        {
+        try {
             config = new PropertiesConfiguration(propsFile);
             config.setProperty(propertyKey, propertyValue);
             config.save();
-        }
-        catch(ConfigurationException e)
-        {
-            System.err.println((new StringBuilder("Error writing to properties file ")).append(propsFile).append(" with key ").append(propertyKey).toString());
+        } catch (ConfigurationException e) {
+            System.err.println("Error writing to properties file " + propsFile + " with key " + propertyKey);
             System.err.println(e.getLocalizedMessage());
         }
     }
 
-    public String addVersionWhere(boolean addWhere)
-    {
+
+    public String addVersionWhere(boolean addWhere) {
         String messageResources = getMessageResource();
+
         String whereClause = " ";
         String versionId = getProperty(messageResources, "defaultVersion");
-        if(!StringUtils.isNullOrEmpty(versionId) && versionId.startsWith("FIFA"))
-        {
-            if(addWhere)
+
+        if (!StringUtils.isNullOrEmpty(versionId) && versionId.startsWith("FIFA")) {
+            if (addWhere) {
                 whereClause = " WHERE ";
-            else
+            } else {
                 whereClause = " AND ";
-            whereClause = (new StringBuilder(String.valueOf(whereClause))).append("s.versionId = '").append(versionId).append("' ").toString();
+            }
+            whereClause = String.valueOf(whereClause) + "s.versionId = '" + versionId + "' ";
         }
         return whereClause;
     }
 
-    public String getMessageResource()
-    {
+    public String getMessageResource() {
         String messageResource = null;
         String hostName = getHostName();
-        if(hostName.equalsIgnoreCase("SERVER2012"))
+        if (hostName.equalsIgnoreCase("SERVER2012")) {
             messageResource = "../webapps/FIFA_JSF/WEB-INF/classes/resources/messageresources.properties";
-        else
-        if(hostName.equalsIgnoreCase("nwilsonvu1"))
+        }
+        else if (hostName.equalsIgnoreCase("nwilsonvu1")) {
             messageResource = "/home/nwilson/Documents/FIFA_JSF/build/classes/resources/messageresources.properties";
-        else
+        } else {
             messageResource = "/home/nick/workspace/FIFA_JSF/build/classes/resources/messageresources.properties";
+        }
+
         return messageResource;
     }
 
-    public String getFactResource()
-    {
+
+    public String getFactResource() {
         String factResource = null;
         String hostName = getHostName();
-        if(hostName.equalsIgnoreCase("SERVER2012"))
+        if (hostName.equalsIgnoreCase("SERVER2012")) {
             factResource = "../webapps/FIFA_JSF/WEB-INF/classes/resources/factresources.properties";
-        else
-        if(hostName.equalsIgnoreCase("nwilsonvu1"))
+        }
+        else if (hostName.equalsIgnoreCase("nwilsonvu1")) {
             factResource = "/home/nwilson/Documents/FIFA_JSF/build/classes/resources/factresources.properties";
-        else
+        } else {
             factResource = "/home/nick/workspace/FIFA_JSF/build/classes/resources/factresources.properties";
+        }
+
         return factResource;
     }
 
-    public String getDatabaseResource()
-    {
-        String hostName = getHostName();
-        String databaseResource;
-        if(hostName.equalsIgnoreCase("SERVER2012"))
+
+    public String getDatabaseResource() {
+        String databaseResource, hostName = getHostName();
+        if (hostName.equalsIgnoreCase("SERVER2012")) {
             databaseResource = "../webapps/FIFA_JSF/WEB-INF/classes/resources/database.properties";
-        else
-        if(hostName.equalsIgnoreCase("nwilsonvu1"))
+        }
+        else if (hostName.equalsIgnoreCase("nwilsonvu1")) {
             databaseResource = "/home/nwilson/Documents/FIFA_JSF/build/classes/resources/database.properties";
-        else
+        } else {
             databaseResource = "/home/nick/workspace/FIFA_JSF/build/classes/resources/database.properties";
+        }
+
+
         return databaseResource;
     }
 
-    String getHostName()
-    {
+
+
+
+    String getHostName() {
         String hostName = null;
-        try
-        {
+        try {
             hostName = InetAddress.getLocalHost().getHostName();
-        }
-        catch(UnknownHostException e)
-        {
-            System.err.println((new StringBuilder("Hostname not found: ")).append(e.getLocalizedMessage()).toString());
+        } catch (UnknownHostException e) {
+            System.err.println("Hostname not found: " + e.getLocalizedMessage());
             System.err.println(e.getLocalizedMessage());
         }
         return hostName;
     }
 
-    public String getDatabaseEnvironment()
-    {
+    public String getDatabaseEnvironment() {
         String hostName = getHostName();
         String dbEnv = "";
-        if(!hostName.equalsIgnoreCase("SERVER2012"))
+
+        if (!hostName.equalsIgnoreCase("SERVER2012")) {
             dbEnv = getProperty(getDatabaseResource(), "environment");
+        }
+
         return dbEnv.toUpperCase();
     }
 
-    private static void autoBuildFacts()
-    {
+
+    private static void autoBuildFacts() {
         int delay = 5000;
-        int interval = 0xdbba0;
+        int interval = 900000;
         Timer timer = new Timer();
         FacesContext context = FacesContext.getCurrentInstance();
-        final VersionBean versionBean = (VersionBean)context.getApplication().evaluateExpressionGet(context, "#{versionBean}", fifa/jsf/VersionBean);
+        final VersionBean versionBean = (VersionBean)context.getApplication().evaluateExpressionGet(context, "#{versionBean}", VersionBean.class);
         timer.scheduleAtFixedRate(new TimerTask() {
-
-            public void run()
-            {
+            public void run() {
                 versionBean.buildAllFacts(false);
             }
-
-            private final VersionBean val$versionBean;
-
-            
-            {
-                versionBean = versionbean;
-                super();
-            }
-        }
-, delay, interval);
+        },  delay, interval);
     }
-
-    private static PropertiesUtilities instance = null;
-
 }
