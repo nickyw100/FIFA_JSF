471ff52160d3ac4301ed6482b9070f22d9a1e014
Nick <nick_wilson@yahoo.com>
2019-06-27 13:12:48 -0600

    Using better decompiler now!

--- src/fifa/jsf/StatsBean.java
+++ src/fifa/jsf/StatsBean.java
@@ -4,21 +4,19 @@ import fifa.dao.CountryDao;
 import fifa.dao.StatsDao;
 import fifa.dao.TeamDao;
 import fifa.utilities.PropertiesUtilities;
-import java.io.Serializable;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.EnumSet;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import org.apache.commons.lang3.ArrayUtils;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
-import org.apache.commons.lang3.ArrayUtils;
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
 
 @ManagedBean
 @ViewScoped
-public class StatsBean extends AbstractResultBean
-{
+public class StatsBean
+        extends AbstractResultBean {
     private static final long serialVersionUID = -4163535146058707662L;
     private String countryId;
     private String countryName;
@@ -30,15 +28,11 @@ public class StatsBean extends AbstractResultBean
     private boolean matchAbandoned;
     private String lastPlayerNameAdded;
 
-
-    private String value;
-    private boolean selectable;
-
     public StatsBean() {
         StatsDao statsDao = new StatsDao();
         List<LastResultBean> lastResultList = statsDao.getLastResult();
         if (lastResultList != null) {
-            LastResultBean lastResult = (LastResultBean)lastResultList.get(0);
+            LastResultBean lastResult = lastResultList.get(0);
             this.gameType = lastResult.getGameType();
             this.division = lastResult.getDivision();
             this.opponentDivision = lastResult.getDivision();
@@ -59,7 +53,7 @@ public class StatsBean extends AbstractResultBean
 
         for (int x = 0; x < arrayOfHomeAwayEnum.length; x++) {
             if (!arrayOfHomeAwayEnum[x].isSelectable()) {
-                arrayOfHomeAwayEnum = (HomeAwayEnum[])ArrayUtils.removeElement(arrayOfHomeAwayEnum, arrayOfHomeAwayEnum[x]);
+                arrayOfHomeAwayEnum = ArrayUtils.removeElement(arrayOfHomeAwayEnum, arrayOfHomeAwayEnum[x]);
             }
         }
 
@@ -71,22 +65,30 @@ public class StatsBean extends AbstractResultBean
 
         for (int x = 0; x < arrayOfGameTypeEnum.length; x++) {
             if (!arrayOfGameTypeEnum[x].isSelectable()) {
-                arrayOfGameTypeEnum = (GameTypeEnum[])ArrayUtils.removeElement(arrayOfGameTypeEnum, arrayOfGameTypeEnum[x]);
+                arrayOfGameTypeEnum = ArrayUtils.removeElement(arrayOfGameTypeEnum, arrayOfGameTypeEnum[x]);
             }
         }
 
         return arrayOfGameTypeEnum;
     }
 
-    public String getTeamId() { return this.teamId; }
 
-    public void setTeamId(String teamId) { this.teamId = teamId; }
+    public String getTeamId() {
+        return this.teamId;
+    }
+
+
+    public void setTeamId(String teamId) {
+        this.teamId = teamId;
+    }
+
 
     public String getTeamName() {
         if (this.teamName == null) {
             TeamDao teamDao = new TeamDao();
             this.teamName = teamDao.getTeamName(this.countryId, this.teamId);
         }
+
         return this.teamName;
     }
 
@@ -96,20 +98,35 @@ public class StatsBean extends AbstractResultBean
         return sdf.format(date);
     }
 
-    public int getDivision() { return this.division; }
 
-    public void setDivision(int division) { this.division = division; }
+    public int getDivision() {
+        return this.division;
+    }
 
-    public boolean isMatchAbandoned() { return this.matchAbandoned; }
 
-    public void setMatchAbandoned(boolean matchAbandoned) { this.matchAbandoned = matchAbandoned; }
+    public void setDivision(int division) {
+        this.division = division;
+    }
 
-    public String getCountryId() { return this.countryId; }
 
-    public void setCountryId(String countryId) { this.countryId = countryId; }
+    public boolean isMatchAbandoned() {
+        return this.matchAbandoned;
+    }
 
-    public void setCountryName(String countryName) { this.countryName = countryName; }
 
+    public void setMatchAbandoned(boolean matchAbandoned) {
+        this.matchAbandoned = matchAbandoned;
+    }
+
+
+    public String getCountryId() {
+        return this.countryId;
+    }
+
+
+    public void setCountryId(String countryId) {
+        this.countryId = countryId;
+    }
 
     public String getCountryName() {
         if (this.countryName == null) {
@@ -119,24 +136,55 @@ public class StatsBean extends AbstractResultBean
         return this.countryName;
     }
 
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
 
-    public String getLastPlayerNameAdded() { return this.lastPlayerNameAdded; }
+    public String getLastPlayerNameAdded() {
+        return this.lastPlayerNameAdded;
+    }
 
-    public void setLastPlayerNameAdded(String lastPlayerNameAdded) { this.lastPlayerNameAdded = lastPlayerNameAdded; }
 
-    public int getOpponentDivision() { return this.opponentDivision; }
+    public void setLastPlayerNameAdded(String lastPlayerNameAdded) {
+        this.lastPlayerNameAdded = lastPlayerNameAdded;
+    }
 
-    public void setOpponentDivision(int opponentDivision) { this.opponentDivision = opponentDivision; }
 
-    public HomeAwayEnum getHomeAway() { return this.homeAway; }
+    public int getOpponentDivision() {
+        return this.opponentDivision;
+    }
 
-    public void setHomeAway(HomeAwayEnum homeAway) { this.homeAway = homeAway; }
 
-    public enum HomeAwayEnum
-    {
+    public void setOpponentDivision(int opponentDivision) {
+        this.opponentDivision = opponentDivision;
+    }
+
+
+    public HomeAwayEnum getHomeAway() {
+        return this.homeAway;
+    }
+
+
+    public void setHomeAway(HomeAwayEnum homeAway) {
+        this.homeAway = homeAway;
+    }
+
+    public GameTypeEnum getGameType() {
+        return this.gameType;
+    }
+
+    public void setGameType(GameTypeEnum gameType) {
+        this.gameType = gameType;
+    }
+
+
+    public enum HomeAwayEnum {
         Home("H", true), Away("A", true), Both("B", false);
-        static  {
-            Map<String, GameTypeEnum>  valueMap = new HashMap();
+
+        private static final Map<String, HomeAwayEnum> valueMap;
+
+        static {
+            valueMap = new HashMap();
 
 
             for (HomeAwayEnum instance : EnumSet.allOf(HomeAwayEnum.class))
@@ -152,7 +200,7 @@ public class StatsBean extends AbstractResultBean
         }
 
         public static HomeAwayEnum findByValue(String value) {
-            HomeAwayEnum type = (HomeAwayEnum)valueMap.get(value);
+            HomeAwayEnum type = valueMap.get(value);
             if (type != null) {
                 return type;
             }
@@ -160,49 +208,59 @@ public class StatsBean extends AbstractResultBean
         }
 
 
-        public String getValue() { return this.value; }
+        public String getValue() {
+            return this.value;
+        }
 
 
-
-        public void setValue(String value) { this.value = value; }
-
-
-
-        public boolean isSelectable() { return this.selectable; }
+        public void setValue(String value) {
+            this.value = value;
+        }
 
 
+        public boolean isSelectable() {
+            return this.selectable;
+        }
 
-        public void setSelectable(boolean selectable) { this.selectable = selectable; }
+
+        public void setSelectable(boolean selectable) {
+            this.selectable = selectable;
+        }
     }
 
 
     public enum SearchCriteriaEnum
-            implements Serializable
-    {
+            implements Serializable {
         Greater(">"), LessThan("<"), Equals("=");
 
         private final String label;
 
 
-        SearchCriteriaEnum(String label) { this.label = label; }
+        SearchCriteriaEnum(String label) {
+            this.label = label;
+        }
 
 
-
-        public String getLabel() { return this.label; }
+        public String getLabel() {
+            return this.label;
+        }
     }
 
 
-
-    public enum GameTypeEnum
-    {
+    public enum GameTypeEnum {
         All("A", false), Friendly("F", true), Season("S", true), Cup("C", true);
-        static  {
-            valueMap = new HashMap();
 
+        private static final Map<String, GameTypeEnum> valueMap;
+
+        static {
+            valueMap = new HashMap();
 
             for (GameTypeEnum instance : EnumSet.allOf(GameTypeEnum.class))
                 valueMap.put(instance.getValue(), instance);
         }
+
+        private String value;
+        private boolean selectable;
 
         GameTypeEnum(String value, boolean selectable) {
             this.value = value;
@@ -210,7 +268,7 @@ public class StatsBean extends AbstractResultBean
         }
 
         public static GameTypeEnum findByValue(String value) {
-            GameTypeEnum type = (GameTypeEnum)valueMap.get(value);
+            GameTypeEnum type = valueMap.get(value);
             if (type != null) {
                 return type;
             }
@@ -218,27 +276,23 @@ public class StatsBean extends AbstractResultBean
         }
 
 
-        public String getValue() { return this.value; }
+        public String getValue() {
+            return this.value;
+        }
 
 
-
-        public void setValue(String value) { this.value = value; }
-
-
-
-        public boolean isSelectable() { return this.selectable; }
+        public void setValue(String value) {
+            this.value = value;
+        }
 
 
+        public boolean isSelectable() {
+            return this.selectable;
+        }
 
-        public void setSelectable(boolean selectable) { this.selectable = selectable; }
+
+        public void setSelectable(boolean selectable) {
+            this.selectable = selectable;
+        }
     }
-
-
-
-
-    public GameTypeEnum getGameType() { return this.gameType; }
-
-
-
-    public void setGameType(GameTypeEnum gameType) { this.gameType = gameType; }
 }
